c2905a2711e9cb8edb0806c28767a31aea639d97
Senthil Athiban <senthilathiban0@gmail.com>
2025-01-26 23:40:22 +0530

    add_ISP

--- SOLID/src/services/payment.service.ts
+++ SOLID/src/services/payment.service.ts
@@ -1,123 +1,144 @@
 // Base payment interface - common functionality
 interface IPaymentMethods {
-    processPayment(paymentDetails: any): Promise<any>,
-    getPaymentDetails(paymentId: string): Promise<any>,
-    getPaymentStatus(paymentId: string): Promise<any>,
+  processPayment(paymentDetails: any): Promise<any>;
+  getPaymentDetails(paymentId: string): Promise<any>;
+  getPaymentStatus(paymentId: string): Promise<any>;
+}
+
+// Separate interface for discount-capable payments
+interface IDiscountCapable {
+  calculateDiscount(amount: number): number;
+  isDiscountEligible(productId: string): boolean;
 }
 
 // Specific payment interface - Inteface seggregating principle (ISP) in action
-interface ICreditCardPayment extends IPaymentMethods {
-    getCreditCardNumber(): string,
+interface ICreditCardPayment extends IPaymentMethods, IDiscountCapable {
+  getCreditCardNumber(): string;
 }
 interface IPayPalPayment extends IPaymentMethods {
-    getPayPalAccount(): any,
+  getPayPalAccount(): any;
 }
 interface IStripePayment extends IPaymentMethods {
-    getStripeID(): string,
+  getStripeID(): string;
 }
 
 // Factory interface - Depdendency inversion principle (DIP) in action
 interface IPaymentMethodFactory {
-    getPayment(type: 'credit_card' | 'stripe' | 'paypal'): IPaymentMethods
+  getPayment(type: "credit_card" | "stripe" | "paypal"): IPaymentMethods;
 }
 
 export class PaymentMethodFactory implements IPaymentMethodFactory {
-    public getPayment(type: 'credit_card' | 'stripe' | 'paypal') {
-        switch(type) {
-            case 'credit_card':
-                return new CreditCardPayment();
-            case 'paypal':
-                return new PaypalPayment();
-            case 'stripe':
-                return new StripePayment();
-            default:
-                throw new Error(`No payment system has been found named as ${type}`);
-        }
+  public getPayment(type: "credit_card" | "stripe" | "paypal") {
+    switch (type) {
+      case "credit_card":
+        return new CreditCardPayment();
+      case "paypal":
+        return new PaypalPayment();
+      case "stripe":
+        return new StripePayment();
+      default:
+        throw new Error(`No payment system has been found named as ${type}`);
     }
+  }
 }
 
 // Payment method implementation - Open/close priciple in action ( each payment method allowed for extension, but not allowed for modification)
 export class CreditCardPayment implements ICreditCardPayment {
+  private creditCardNumber: string = "41111111111111";
 
-    private creditCardNumber: string = '41111111111111';
+  public async processPayment(paymentDetails: any) {
+    console.log(
+      `Processing credit card payments for ${paymentDetails.currency} ${paymentDetails.amount}`
+    );
+    await new Promise((r) => setTimeout(r, 2000));
+    console.log("Credit card payment processed successfully");
+  }
 
-    public async processPayment (paymentDetails: any) {
-        console.log(`Processing credit card payments for ${paymentDetails.currency} ${paymentDetails.amount}`);
-        await new Promise(r => setTimeout(r, 2000));
-        console.log('Credit card payment processed successfully');
-    };
+  public async getPaymentDetails(paymentId: string): Promise<string> {
+    return `Credit card details for ID: ${paymentId}`;
+  }
 
-    public async getPaymentDetails(paymentId: string): Promise<string> {
-        return `Credit card details for ID: ${paymentId}`;
-    }
+  public async getPaymentStatus(paymentId: string): Promise<string> {
+    return `Credit card payment status for ID: ${paymentId} is successful`;
+  }
 
-    public async getPaymentStatus(paymentId: string): Promise<string> {
-        return `Credit card payment status for ID: ${paymentId} is successful`;
-    }
+  public getCreditCardNumber(): string {
+    return this.creditCardNumber;
+  }
 
-    public getCreditCardNumber(): string {
-        return this.creditCardNumber;
-    }
+  isDiscountEligible(productId: string): boolean {
+      return true;
+  }
+
+  calculateDiscount(amount: number): number {
+    return 0.15 * amount;    
+  }
 }
 
 export class StripePayment implements IStripePayment {
-    private stripeID: string = 'stripe_123456';
+  private stripeID: string = "stripe_123456";
 
-    public async processPayment (paymentDetails: any): Promise<void> {
-        console.log(`Processing Stripe payment for ${paymentDetails.currency} ${paymentDetails.amount}`);
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        console.log('Stripe payment processed successfully');
-    };
+  public async processPayment(paymentDetails: any): Promise<void> {
+    console.log(
+      `Processing Stripe payment for ${paymentDetails.currency} ${paymentDetails.amount}`
+    );
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log("Stripe payment processed successfully");
+  }
 
-    public async getPaymentDetails(paymentId: string): Promise<string> {
-        return `Stripe payment details for ID: ${paymentId}`;
-    }
+  public async getPaymentDetails(paymentId: string): Promise<string> {
+    return `Stripe payment details for ID: ${paymentId}`;
+  }
 
-    public async getPaymentStatus(paymentId: string): Promise<string> {
-        return `Stripe payment status for ID: ${paymentId} is successfull`;
-    }
+  public async getPaymentStatus(paymentId: string): Promise<string> {
+    return `Stripe payment status for ID: ${paymentId} is successfull`;
+  }
 
-    public getStripeID(): string {
-        return this.stripeID;
-    }
+  public getStripeID(): string {
+    return this.stripeID;
+  }
 }
 
 export class PaypalPayment implements IPayPalPayment {
-    private paymentId: string = 'paypa_account_123';
+  private paymentId: string = "paypa_account_123";
 
-    public async processPayment (paymentDetails: any): Promise<void> {
-        console.log(`Processing paypal payments for ${paymentDetails.currency} ${paymentDetails.amount}`);
-        await new Promise(r => setTimeout(r, 2000));
-        console.log(`Paypal payment processed successfully`);
-    };
+  public async processPayment(paymentDetails: any): Promise<void> {
+    console.log(
+      `Processing paypal payments for ${paymentDetails.currency} ${paymentDetails.amount}`
+    );
+    await new Promise((r) => setTimeout(r, 2000));
+    console.log(`Paypal payment processed successfully`);
+  }
 
-    public async getPaymentDetails(paymentId: string): Promise<string> {
-        return `Paypal payment details for ID: ${paymentId}`;
-    }
+  public async getPaymentDetails(paymentId: string): Promise<string> {
+    return `Paypal payment details for ID: ${paymentId}`;
+  }
 
-    public async getPaymentStatus(paymentId: string) {
-        return `Paypal payment status for ${paymentId} is successfull`;
-    }
+  public async getPaymentStatus(paymentId: string) {
+    return `Paypal payment status for ${paymentId} is successfull`;
+  }
 
-    public getPayPalAccount() : string {
-        return this.paymentId;
-    }
+  public getPayPalAccount(): string {
+    return this.paymentId;
+  }
 }
 
 // PaymentService depends on abstraction (IPaymentMethodFactory and IPaymentMethods)
 // rather than concrete implementations, demonstrating Dependency Inversion
 export class PaymentService {
-    private paymentMethod: IPaymentMethods;
-    constructor(private readonly paymentFactory: IPaymentMethodFactory, paymentMethod: 'credit_card' | 'stripe' | 'paypal') {
-        this.paymentMethod = this.paymentFactory.getPayment(paymentMethod);
-    }
+  private paymentMethod: IPaymentMethods;
+  constructor(
+    private readonly paymentFactory: IPaymentMethodFactory,
+    paymentMethod: "credit_card" | "stripe" | "paypal"
+  ) {
+    this.paymentMethod = this.paymentFactory.getPayment(paymentMethod);
+  }
 
-    public processPayment(paymentDetails: any) {
-        return this.paymentMethod.processPayment(paymentDetails);
-    }
+  public processPayment(paymentDetails: any) {
+    return this.paymentMethod.processPayment(paymentDetails);
+  }
 
-    public getPaymentDetails(paymentId: string) {
-        return this.paymentMethod.getPaymentDetails(paymentId);
-    }
-
+  public getPaymentDetails(paymentId: string) {
+    return this.paymentMethod.getPaymentDetails(paymentId);
+  }
 }
